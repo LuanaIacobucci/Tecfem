6526f8d643515bfdba697c3d45b83c31ec9d66d2
VictoriaAndrea <91353425+VickyyKiwii@users.noreply.github.com>
2022-05-17 19:49:36 -0400

    commit vicky

--- controlador/validacion_login.php
+++ controlador/validacion_login.php
@@ -1,28 +1,5 @@
 <?php 
 
-include('conectar.php');
 
-session_start();
-
-$nombreUsuario = $_POST["nombreUsuario"]; 
-$contrasena = $_POST["contrasena"]; 
-
-$sql = "SELECT*FROM cliente WHERE nombreUsuario = '$nombreUsuario' AND contrasena = $contrasena ";
-$resultado = $conectar->query($sql);
-
-$row = $resultado->fetch_assoc();
-
-if($row['nombreUsuario'] == $nombre && $row['contrasena'] == $contrasena){
-$_SESSION['nombreUsuario'] = $nombreUsuario;
-header("Location:#"); 
-}else{
-header("Location: login.php");
-}
-
-if($resultado->num_rows > 0){
-echo 'Usuaria ha ingresado correctamente';
-}else{
-echo 'Usuaria no existente';
-} 
 
 ?>
