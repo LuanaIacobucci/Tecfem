ae540490cfb2eda8dd8d8cbae91277515fd2711e
VictoriaAndrea <91353425+VickyyKiwii@users.noreply.github.com>
2022-05-18 00:25:52 -0400

    validacion login vicky

--- controlador/validacion_login.php
+++ controlador/validacion_login.php
@@ -1,10 +1,27 @@
 <?php 
- 
+
  include('conectar.php')
 
  $NOMUSUARIO=$_POST['nombreUsuario'];
  $CONTRASENA=$_POST['contrasena']; 
 
-$consulta = "SELECT * FROM cliente"
+$consulta = "SELECT * FROM cliente where nombreUsuario = '$NOMUSUARIO' and contrasena = '$CONTRASENA' ";
+$resultado = mysql_query($conexion, $consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+header("location: #");
+
+}else{
+header("location:login.php");
+?>
+
+<h3>Error de autentificacion</h3>
+<?php
+}
+
+mysqli_free_result($resultado);
+mysqli_close($conexion);
 
 ?>
