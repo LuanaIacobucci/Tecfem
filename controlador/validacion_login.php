9601d8664abed2726cecc304620af3aa39872e09
VictoriaAndrea <91353425+VickyyKiwii@users.noreply.github.com>
2022-05-18 23:09:53 -0400

    commit validacion vic

--- controlador/validacion_login.php
+++ controlador/validacion_login.php
@@ -1,27 +1,23 @@
 <?php 
 
-include('modelo\conectar.php');
-
 session_start();
+
+require "modelo\conectar.php";
+
+$conectar = conectar();
 
 $nombreUsuario = $_POST["nombreUsuario"]; 
 $contrasena = $_POST["contrasena"]; 
 
-$sql = "SELECT*FROM cliente WHERE nombreUsuario = '$nombreUsuario' AND contrasena = $contrasena ";
-$resultado = $conectar->query($sql);
+$sql = "SELECT * FROM cliente WHERE nombreUsuario = '$nombreUsuario' AND contrasena = $contrasena ";
+$result = mysqli_query($conectar,$sql);
 
-$row = $resultado->fetch_assoc();
-
-if($row['nombreUsuario'] == $nombre && $row['contrasena'] == $contrasena){
-$_SESSION['nombreUsuario'] = $nombreUsuario;
+if(mysqli_num_rows($result)>0){
 header("Location:index.php");
 }else{
-header("Location: login.php");
+header("Location:login.php");
+echo:"Error al ingresar datos";
+
 }
 
-if($resultado->num_rows > 0){
-echo 'Usuaria ha ingresado correctamente';
-}else{
-echo 'Usuaria no existente';
-} 
 ?>
