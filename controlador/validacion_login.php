461a2c705d0349ad3a9f334dfe7655d081ae6db4
VictoriaAndrea <91353425+VickyyKiwii@users.noreply.github.com>
2022-05-18 20:45:46 -0400

    commit vicky validacion

--- controlador/validacion_login.php
+++ controlador/validacion_login.php
@@ -1,12 +1,13 @@
 <?php 
 session_start();
 
- include('modelo\conectar.php');
+$conn= mysqli_connect("localhost", "root", "root", "tecfem");
 
  $NOMBREUSUARIO=$_POST['nombreUsuario'];
  $PASSWORD=$_POST['contrasena']; 
 
-$consulta = "SELECT * FROM cliente where nombreUsuario = '$NOMBREUSUARIO' and contrasena = '$PASSWORD' ";
+$consulta = mysqli_query("SELECT * FROM cliente where nombreUsuario = '$NOMBREUSUARIO' and contrasena = '$PASSWORD' ");
+
 $resultado = mysql_query($conexion, $consulta);
 
 $filas=mysqli_num_rows($resultado);
