a8d263079c5a8719ee576a3742e2b0ab29eac868
VictoriaAndrea <91353425+VickyyKiwii@users.noreply.github.com>
2022-05-20 18:24:53 -0400

    commit vick

--- controlador/validacion_login.php
+++ controlador/validacion_login.php
@@ -4,11 +4,6 @@
 $nombreUsuario = $_POST['nombreUsuario'];
 $password = $_POST['contrasena'];
 
-if(empty($nombreUsuario) || empty($password)){
-header('Location:login.php');
-exit();
-}
-
 $sql = "SELECT*FROM cliente WHERE nombreUsuario = '$nombreUsuario' AND contrasena = $password ";
 $resultado = $conn->query($sql);
 
