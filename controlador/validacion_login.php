96864ef9f1cd3dab5587138bf8dcb8efdf43dd93
VictoriaAndrea <91353425+VickyyKiwii@users.noreply.github.com>
2022-05-20 11:35:22 -0400

    commit vicky

--- controlador/validacion_login.php
+++ controlador/validacion_login.php
@@ -10,9 +10,6 @@ header("Location: index.html");
 exit();
 }
 
-mysql_connect('localhost','root','root') or die("Error al conectar " . mysql_error());
-mysql_select_db('login') or die ("Error al seleccionar la Base de datos: " . mysql_error());
-
 $sql = mysql_query("SELECT * FROM cliente WHERE $usuario='nombreUsuario' AND $pass='contrasena'");
 
 if($row = mysql_fetch_array($sql)){
