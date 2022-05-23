4f801960acddd87a139609a88f66d1ce3b7b6805
VictoriaAndrea <91353425+VickyyKiwii@users.noreply.github.com>
2022-05-22 23:42:14 -0400

    commit vicky name

--- registro.php
+++ registro.php
@@ -56,7 +56,7 @@
               <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
               <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
             </svg>
-           <label><input type="checkbox" id="cboxcliente" value="checkbox1"></label><br>
+           <label><input type="checkbox" id="cboxcliente" value="checkbox1" name="cuentacliente"></label><br>
              <p>Cliente</p>
         </div> <!----Fin Columna Cliente---->
         <div class="col"> <!---Columna Checkbox Proveedor de servicio--->
@@ -64,7 +64,7 @@
             <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
             <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
           </svg>
-          <label><input type="checkbox" id="cboxproveedor" value="checkbox2"></label><br>
+          <label><input type="checkbox" id="cboxproveedor" value="checkbox2" name="cuentaproveedor"></label><br>
 
               <p>Proveedor de Servicio</p>
                 </div>
