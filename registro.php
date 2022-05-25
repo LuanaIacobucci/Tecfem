7ded5d989b45c4f5a36ff8f67b8818620e778b19
VictoriaAndrea <91353425+VickyyKiwii@users.noreply.github.com>
2022-05-25 13:31:06 -0400

    commit tipo cuenta radiobutton

--- registro.php
+++ registro.php
@@ -58,7 +58,7 @@
               <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
               <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
             </svg>
-           <label><input type="checkbox" id="cboxcliente" value="checkbox1" name="cuentacliente"></label><br>
+           <label><input type="radio" id="cliente" value="cliente" name="tipocuenta"></label><br>
              <p>Cliente</p>
         </div> <!----Fin Columna Cliente---->
         <div class="col"> <!---Columna Checkbox Proveedor de servicio--->
@@ -66,8 +66,7 @@
             <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
             <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
           </svg>
-          <label><input type="checkbox" id="cboxproveedor" value="checkbox2" name="cuentaproveedor"></label><br>
-
+          <label><input type="radio" id="proveedor" value="proveedor" name="tipocuenta"></label><br>
               <p>Proveedor de Servicio</p>
                 </div>
   </div> <!----Fin fila----->
